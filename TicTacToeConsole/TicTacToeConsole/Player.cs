638073dd70a6a027bfb777420045e0b2292d753a
Wiktor Szczeszek <58635705+wiQ1999@users.noreply.github.com>
2020-09-22 22:00:21 +0200

    Add project

---
+++ TicTacToeConsole/TicTacToeConsole/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeConsole
+{
+	class Player
+	{
+		public string Name { get; set; }
+		public int Value { get; set; }
+
+		public Player(string Name, int Value)
+		{
+			this.Name = Name;
+			this.Value = Value;
+		}
+
+		public Char GetChar()
+		{
+			switch (Value)
+			{
+				case 1:
+					return 'O';
+				case -1:
+					return 'X';
+			}
+			return ' ';
+		}
+
+		public string GetCharName()
+		{
+			switch (Value)
+			{
+				case 1:
+					return "KÓLKO";
+				case -1:
+					return "KŻYRZYK";
+			}
+			return " ";
+		}
+	}
+}
